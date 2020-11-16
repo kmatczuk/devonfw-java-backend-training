4b38bfc8cc6740a3ddd4fb16cc26cbac4beb52c4
Kamil Matczuk <kamil.matczuk@capgemini.com>
2020-11-16 16:42:32 +0100

    empty comments cleaned up

--- order-service/core/src/main/java/com/devonfw/app/java/order/orderservice/dataaccess/api/ItemEntity.java
+++ order-service/core/src/main/java/com/devonfw/app/java/order/orderservice/dataaccess/api/ItemEntity.java
@@ -16,54 +16,36 @@ public class ItemEntity extends ApplicationPersistenceEntity implements Item {
 
   private static final long serialVersionUID = 1L;
 
-  /**
-   * @return name
-   */
   @Override
   public String getName() {
 
     return this.name;
   }
 
-  /**
-   * @param name new value of {@link #getname}.
-   */
   @Override
   public void setName(String name) {
 
     this.name = name;
   }
 
-  /**
-   * @return description
-   */
   @Override
   public String getDescription() {
 
     return this.description;
   }
 
-  /**
-   * @param description new value of {@link #getdescription}.
-   */
   @Override
   public void setDescription(String description) {
 
     this.description = description;
   }
 
-  /**
-   * @return price
-   */
   @Override
   public Double getPrice() {
 
     return this.price;
   }
 
-  /**
-   * @param price new value of {@link #getprice}.
-   */
   @Override
   public void setPrice(Double price) {
 
